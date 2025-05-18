05b2ba4c97379c3014cfc670eda45894acca6837
aaaaaa <aaaaaa@WINDOWS-TOTE>
2025-05-17 21:27:29 -0600

    Agregar archivos de proyecto.

---
+++ Examen2/Servivios/Cancion.cs
@@ -0,0 +1,25 @@
+﻿public class Cancion
+{
+    // Propiedades
+    public string Nombre { get; set; }
+    public string Artista { get; set; }
+    public int DuracionSegundos { get; set; }
+
+    // Constructor
+    public Cancion(string nombre, string artista, int duracionSegundos)
+    {
+        Nombre = nombre;
+        Artista = artista;
+        DuracionSegundos = duracionSegundos;
+    }
+
+    // Métodos
+    public override string ToString()
+    {
+        // Calculo de minutos y segundos
+        int minutos = DuracionSegundos / 60;
+        int segundos = DuracionSegundos % 60;
+        // Devolucion del string
+        return ($"{Nombre} - {Artista} ({minutos}:{segundos})");
+    }
+}
---
+++ Examen2/Servivios/Usuario.cs
@@ -0,0 +1,40 @@
+﻿public class Usuario
+{
+    // Propiedades
+    public string Nombre { get; set; }
+    public Dictionary<string, List<Cancion>> ListasReproduccion { get; set; }
+
+    // Constructor
+    public Usuario(string nombre)
+    {
+        Nombre = nombre;
+        ListasReproduccion = new Dictionary<string, List<Cancion>>();
+    }
+
+    // Métodos
+    public void CrearListaReproduccion(string nombreLista)
+    {
+        if (!ListasReproduccion.ContainsKey(nombreLista)) // Verifica si la lista no existe
+        {
+            ListasReproduccion[nombreLista] = new List<Cancion>();
+            Console.WriteLine($"Lista de reproducción '{nombreLista}' creada con éxito.");
+        }
+        else
+        {
+            Console.WriteLine("ERROR: La lista de reproducción ya existe.");
+        }
+    }
+    public void AgregarCancionALista(string nombreLista, Cancion cancion)
+    {
+        if (ListasReproduccion.ContainsKey(nombreLista)) // Verifica si la lista existe
+        {
+            ListasReproduccion[nombreLista].Add(cancion);
+            Console.WriteLine($"Canción '{cancion.Nombre}' agregada a la lista '{nombreLista}'.");
+        }
+        else
+        {
+            Console.WriteLine("ERROR: La lista de reproducción no existe.");
+        }
+    }
+
+}
